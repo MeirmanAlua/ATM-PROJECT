d3c87a5db042a34d0daca31c5c41ebc8f80503fe
MeirmanAlua <80328875+MeirmanAlua@users.noreply.github.com>
2021-03-13 06:03:07 +0600

    Add files via upload

--- Aluaproject/java/AccountService.java
+++ Aluaproject/java/AccountService.java
@@ -1,14 +1,15 @@
 import entity.Account;
 
-public class AccountService {
-    private Connecter parentConnector;
-    public AccountService(Connecter parentConnector){//to get connection to this entity/table
+public class AccountService {  // create Accountservice
+    private Connecter parentConnector; // extended class of connecter
+    public AccountService(Connecter parentConnector) // constructor
+    {//to get connection to this entity/table
         this.parentConnector=parentConnector;
     }
-    public Account find(Object primaryKey){ //first interface method
+    public Account find(Object primaryKey){        //first interface method
         return this.parentConnector.getManager().find(Account.class, primaryKey);
     }
-    public void save(Account object){//second interface method
+    public void save(Account object) {//second interface method to save the acctions with db
         this.parentConnector.getManager().persist(object);
     }
 }
